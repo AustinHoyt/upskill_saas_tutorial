55c72c7c5ac8526deef43fa2b6fdb5e4a5d388e1
Austin Hoyt <vanillahoyt@gmail.com>
2017-02-06 23:15:03 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < Actionmailer::Base
+  default to: 'austinhoyt1@gmail.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
